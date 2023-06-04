b3cc1ab37e1a46b89497311e9b31bc63d64408a7
Andy <82745396+AndyHCode@users.noreply.github.com>
2023-06-04 01:09:55 -0400

    added projects

--- react-portfolio/tailwind.config.js
+++ react-portfolio/tailwind.config.js
@@ -13,6 +13,16 @@ module.exports = {
       fontFamily: {
         burtons: "burtons",
       },
+      colors:{
+        'offWhite':{
+          100: '#F5F0E8',
+          200: '#F0E7DB',
+        },
+        'offDark':{
+          100:'#313134',
+          200:'#202023',
+        },
+      },
     },
   },
   plugins: [],
